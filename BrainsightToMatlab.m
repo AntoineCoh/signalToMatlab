104ecf11205c9d6ac1e55c19d290dc7c0d3de3b3
Math <math.math@caramail.fr>
2025-07-03 15:51:46 -0400

    Brainsight: Clarifying + adding additional functions

--- BrainsightToMatlab.m
+++ BrainsightToMatlab.m
@@ -4,10 +4,15 @@ clear;
 close all;
 
 %% Processing the text
-% Reading the file as a raw text to remove the comments
 filename = 'fichiers_test/MS.txt';
-filetxt = fopen(filename, 'r');
-lines = {};
 
-% Collecting the headers for the target
-targetheaders = findHeaders(filetxt, 7);
+% Collecting the headers for the target (if there is ONE target)
+targetheaders = findHeaders(filename, 7);
+targetvalues = findValues(filename, 8);
+target = cell2struct(targetvalues, targetheaders, 2);
+
+%% Collecting the values
+
+headers = findHeaders(filename, 9);
+values = findValues(filename, 10);
+data = cell2struct(values, headers, 2);
